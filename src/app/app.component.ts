23899d985d22c8f9fc7bcf458dfd46f6d68e0061
sriranjanipandian <sriranjani207@gmail.com>
2020-08-07 15:53:29 +0530

    httpget

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
 import { Component } from '@angular/core';
-
+ 
+import { GitHubService } from './github.service';
+import { repos } from './repos';
+ 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'httpget';
+  userName: string = "tektutorialshub"
+  repos: repos[];
+ 
+  loading: boolean = false;
+  errorMessage;
+ 
+  constructor(private githubService: GitHubService) {
+  }
+ 
+  public getRepos() {
+    this.loading = true;
+    this.errorMessage = "";
+    this.githubService.getRepos(this.userName)
+      .subscribe(
+        (response) => {                           //next() callback
+          console.log('response received')
+          this.repos = response; 
+        },
+        (error) => {                              //error() callback
+          console.error('Request failed with error')
+          this.errorMessage = error;
+          this.loading = false;
+        },
+        () => {                                   //complete() callback
+          console.error('Request completed')      //This is actually not needed 
+          this.loading = false; 
+        })
+  }
 }
